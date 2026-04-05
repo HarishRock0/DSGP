eb271ae6d149e4ff65137508835ef76b31e6745f
Upali Lalith Wijekoon <yasiru.methmal@outlook.com>
2026-04-05 10:06:51 +0530

    Repositary Cleaning [Remove unwanted files] INPROGRESS

--- dataloader/child_case_data_loader.py
+++ dataloader/child_case_data_loader.py
@@ -1,32 +1,3 @@
-# import os
-# import pickle
-# import pandas as pd
-#
-#
-# class ChildCasesDataLoader:
-#     def __init__(self, project_root):
-#         self.project_root = project_root
-#
-#     def load(self):
-#         model_path = os.path.join(self.project_root, "model", "child_case_nlp.pkl")
-#         child_case_path = os.path.join(self.project_root, "data", "childcases.xlsx")
-#
-#         with open(model_path, "rb") as f:
-#             child_case_model = pickle.load(f)
-#
-#         if hasattr(model, 'tokenizer'):
-#             if not hasattr(model.tokenizer, '_pad_token'):
-#                 model.tokenizer._pad_token = model.tokenizer.pad_token
-#
-#         child_data = pd.read_excel(child_case_path)
-#         child_data['Avg_cases'] = child_data.iloc[:, 1:].mean(axis=1)
-#         child_data['text'] = (
-#                 "District " + child_data['District'].astype(str) +
-#                 " has an average of " + child_data['Avg_cases'].astype(str) +
-#                 " child cases"
-#         )
-#
-#         return child_case_model, child_data
 import os
 import re
 import pickle
--- signals/mental_health_nlp_signals.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class MentalHealthNLPSignals(BaseModel):
-    preference: str
-
-class MentalHealthRecommenderSignals(MentalHealthNLPSignals):
-    districts : list
